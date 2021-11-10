5990b675d0ba083c3f4a9fd804dcdad3f7849351
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-11-09 19:18:55 -0500

    Create put.php

---
+++ put.php
@@ -0,0 +1,57 @@
+<?php
+$e = "
+Tue, 18:27:10 zXCV Hax101, 136.56.121.227, 172.70.134.100,  (zXCV/Parity-Client)
+
+Tue, 18:27:33 zXCV Hax101, 136.56.121.227, 172.70.134.100, 1306963744 (zXCV/Parity-Client)
+
+Tue, 18:27:36 zXCV Hax101, 136.56.121.227, 172.70.134.100, 1306963744 (zXCV/Parity-Client)
+
+Tue, 18:27:47 zXCV Hax101, 136.56.121.227, 172.70.134.114, 3110055032 (Mozilla/5.0 (X11; CrOS x86_64 13982.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.162 Safari/537.36)
+
+Tue, 18:27:55 zXCV Hax101, 136.56.121.227, 172.70.134.114, 3110055032 (Mozilla/5.0 (X11; CrOS x86_64 13982.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.162 Safari/537.36)
+
+Tue, 18:29:47 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 172.70.34.165, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 18:29:47 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 172.70.34.165, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 18:29:51 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 172.70.34.165, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 18:31:37 Terry Silver, 2603:6080:640:517:493a:dc8a:964:b54, 172.69.63.21,  (Mozilla/5.0 (X11; CrOS x86_64 14092.77.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.107 Safari/537.36)
+
+Tue, 18:31:48 Terry Silver, 2603:6080:640:517:493a:dc8a:964:b54, 172.69.63.21, 1050258816 (Mozilla/5.0 (X11; CrOS x86_64 14092.77.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.107 Safari/537.36)
+
+Tue, 18:33:30 zXCV Hax101, 136.56.121.227, 172.70.34.84, 3110055032 (Mozilla/5.0 (X11; CrOS x86_64 13982.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.162 Safari/537.36)
+
+Tue, 18:33:45 zXCV Hax101, 136.56.121.227, 172.70.34.84, 3110055032 (Mozilla/5.0 (X11; CrOS x86_64 13982.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.162 Safari/537.36)
+
+Tue, 18:33:49 zXCV Hax101, 136.56.121.227, 172.70.34.84, 3110055032 (Mozilla/5.0 (X11; CrOS x86_64 13982.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.162 Safari/537.36)
+
+Tue, 18:57:54 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 172.70.134.51, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 19:21:35 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 108.162.212.34, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 19:21:36 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 108.162.212.34, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 19:21:38 Moonwatcher, 2603:6080:603:2b00:182f:8457:2bb2:c70a, 108.162.212.34, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 19:32:38 Vaani, 2406:b400:d4:f9e1:d951:d07:342c:5dbe, 172.68.167.42,  (Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15)
+
+Tue, 19:32:41 Vaani, 2406:b400:d4:f9e1:d951:d07:342c:5dbe, 172.68.167.42, 706398659 (Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15)
+
+Tue, 21:29:08 Moonwatcher, 2600:387:2:80f::87, 162.158.187.141,  (Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1)
+
+Tue, 21:29:12 Moonwatcher, 2600:387:2:80f::87, 162.158.187.141, 2246241160 (Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1)
+
+Tue, 21:31:01 Moonwatcher, 2603:6080:603:2b00:e52e:53e9:a6c5:3919, 172.69.63.26,  (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 21:31:01 Moonwatcher, 2603:6080:603:2b00:e52e:53e9:a6c5:3919, 172.69.63.26,  (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 21:31:06 Moonwatcher, 2603:6080:603:2b00:e52e:53e9:a6c5:3919, 172.69.63.26, 2851303742 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)
+
+Tue, 22:36:29 zXCV Hax101, 136.56.121.227, 172.70.34.164,  (zXCV/Parity-Client)
+
+Tue, 22:36:34 zXCV Hax101, 136.56.121.227, 172.70.34.164, 1306963744 (zXCV/Parity-Client)
+";
+
+file_put_contents("./log.txt", $e);
+?>
